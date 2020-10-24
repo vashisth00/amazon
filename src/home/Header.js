e3fe0fab59fc8c9763fae4faf53e5c37b72467eb
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-24 15:22:01 +0530

    Basic screen

--- src/home/Header.js
+++ src/home/Header.js
@@ -1,110 +1,73 @@
-
+/* eslint-disable jsx-a11y/alt-text */
 import '../App.css';
 import logo from './amazon.png';
+import data from './data';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 function Header() {
-    return(
-    <div className="grid-container">
-  <header className="header">
-    <div className="brand">
-      <button onclick="openMenu()">
-        ☰
-      </button>
-      <img src={logo} height="25"></img>
-    </div>
-    <div className="header-links">
-      <a href="cart.html">Cart</a>
-      <a href="signin.html">Sign In</a>
-    </div>
-  </header>
-  <aside className="sidebar">
-    <h3>Shopping Categories</h3>
-    <button className="sidebar-close-button" onclick="closeMenu()">x</button>
-    <ul>
-      <li>
-        <a href="index.html">Pants</a>
-      </li>
-      <li>
-        <a href="index.html">Shirts</a>
-      </li>
-    </ul>
-  </aside>
-  <main className="main">
-    <div className="content">
-      <ul className="products">
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+      }
+      const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open")
+      }
+      return (
+        <BrowserRouter>
+        <div class="grid-container">
+          <header class="header">
+            <div class="brand">
+              <button onClick={openMenu}>
+                &#9776;
+            </button>
+              <a href="index.html"> <img src={logo} height="25"/></a>
             </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
+            <div class="header-links">
+              <a href="cart.html">Cart</a>
+              <a href="signin.html">Sign In</a>
             </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
-            </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
-            </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
-            </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-        <li>
-          <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-              <a href="product.html">Slim Shirt</a>
-            </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </main>
+          </header>
+          <aside class="sidebar">
+            <h3>Shopping Categories</h3>
+            <button class="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+    
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+    
+            </ul>
+          </aside>
+          <main className="main">
+        <div className="products">
+          {
+            data.products.map(product =>
+              <li>
+                <div className="product">
+                  <a href={'/product/' + product._id}>
+                    <img className="product-image" src={product.image} alt="product" />
+
+                  </a>
+                  <div className="product-name">
+                    <a href={'/product/' + product._id}>{product.name}</a>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">${product.price}</div>
+                  <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
+                </div>
+              </li>)
+          }
+
+        </div>
+
+      </main>
   <footer className="footer">
     All right reserved. ADesignGuy Clone 
   </footer>
 </div>
+</BrowserRouter>
 
   );
     }
