117bb7ba9fb7af9b8dfa9bd62d6765a52d7db90b
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-25 14:36:53 +0530

    Unique key

--- src/landing/HomeScreen.js
+++ src/landing/HomeScreen.js
@@ -17,7 +17,7 @@ function HomeScreen(props) {
   return <ul className="products">
     {
       products.map(product =>
-        <li>
+        <li key={product._id}>
           <div className="product">
             <Link to={'/product/' + product._id}>
               <img className="product-image" src={product.image} alt="product" />
