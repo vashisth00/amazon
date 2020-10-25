6f41ba320b355240d6f551b7f4bd0b5d234affc5
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-25 16:19:25 +0530

    Redux state for cart creation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './home/Header';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}> 
     <Header />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
